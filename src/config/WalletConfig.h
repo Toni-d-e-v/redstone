9cf9dde361aa2d659b75168aa90103e8053f9f04
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-05-15 16:12:12 +0100

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -11,10 +11,10 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = "aio";
+    const std::string_view addressPrefix = "R";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
-    const std::string ticker = "AIO";
+    const std::string ticker = "RS";
 
     /* The filename to output the CSV to in save_csv */
     const std::string csvFilename = "transactions.csv";
@@ -24,26 +24,26 @@ namespace WalletConfig
     const std::string addressBookFilename = ".addressBook.json";
 
     /* The name of your deamon */
-    const std::string daemonName = "avriod";
+    const std::string daemonName = "redcoin-node";
 
     /* The name to call this wallet */
-    const std::string walletName = "avrio-wallet";
+    const std::string walletName = "red-wallet";
 
     /* The name of service/walletd, the programmatic rpc interface to a
        wallet */
-    const std::string walletdName = "avrio-service";
+    const std::string walletdName = "red-service";
 
     /* The full name of your crypto */
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
 
     /* Where can your users contact you for support? E.g. discord */
-    const std::string contactLink = "https://discord.gg/kH7gNA9";
+    const std::string contactLink = "https://discord.gg/sy7J6UV";
 
     /* The number of decimals your coin has */
     const uint8_t numDecimalPlaces = CryptoNote::parameters::CRYPTONOTE_DISPLAY_DECIMAL_POINT;
 
     /* The length of a standard address for your coin */
-    const uint16_t standardAddressLength = 98;
+    const uint16_t standardAddressLength = 96;
 
     /* The length of an integrated address for your coin - It's the same as
        a normal address, but there is a paymentID included in there - since
@@ -53,13 +53,13 @@ namespace WalletConfig
     const uint16_t integratedAddressLength = standardAddressLength + ((64 * 11) / 8);
 
     /* The default fee value to use with transactions (in ATOMIC units!) */
-    const uint64_t defaultFee = CryptoNote::parameters::MINIMUM_FEE;
+    const uint64_t defaultFee = 0;
 
     /* The minimum fee value to allow with transactions (in ATOMIC units!) */
-    const uint64_t minimumFee = CryptoNote::parameters::MINIMUM_FEE;
+    const uint64_t minimumFee = 0;
 
     /* The minimum amount allowed to be sent - usually 1 (in ATOMIC units!) */
-    const uint64_t minimumSend = 10; //0.01
+    const uint64_t minimumSend = 1;
 
     /* Is a mixin of zero disabled on your network? */
     const bool mixinZeroDisabled = false;
@@ -70,7 +70,7 @@ namespace WalletConfig
     const uint64_t mixinZeroDisabledAtHeight = CryptoNote::parameters::MIXIN_LIMITS_V2_HEIGHT;
 
     /**
-     * Max size of a post body response - 16MB
+     * Max size of a post body response - 30MB
      * Will decrease the amount of blocks requested from the daemon if this
      * is exceeded.
      * Note - blockStoreMemoryLimit - maxBodyResponseSize should be greater
@@ -78,7 +78,7 @@ namespace WalletConfig
      * Further note: Currently blocks request are not decreased if this is
      * exceeded. Needs to be implemented in future?
      */
-    const size_t maxBodyResponseSize = 1024 * 1024 * 16;
+    const size_t maxBodyResponseSize = 1024 * 1024 * 30;
 
     /**
      * The amount of memory to use storing downloaded blocks - 64MB
